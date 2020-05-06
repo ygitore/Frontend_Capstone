9778219469af95e98b0c79f9d2c2a19f8c44cd75
yitbarekgitore <yitbareky@gmail.com>
2020-05-05 22:02:34 -0500

    one apartment is added to favorites section only once

--- src/components/apartment/Apartment.js
+++ src/components/apartment/Apartment.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react"
-import { Button, Modal, ModalHeader, ModalBody } from "reactstrap"
+import { Button, Modal, ModalHeader, ModalBody, Alert } from "reactstrap"
 import DeleteApartment from "./DeleteApartment"
 import {UserContext} from '../user/UserProvider'
 import AddCommentForm from '../comment/AddCommentForm'
@@ -12,7 +12,7 @@ export default ({apartment}) => {
     const {users} = useContext(UserContext)  
     const {comments} = useContext(CommentContext)
     const {likes} = useContext(LikeContext)
-    const {addFavorite} = useContext(FavoriteContext)
+    const {favorites, addFavorite} = useContext(FavoriteContext)
     const [modal, setModal] = useState(false)
     const toggle = () => setModal(!modal)
 
@@ -21,11 +21,20 @@ export default ({apartment}) => {
 
     
     const addNewApartmentToFavorites = () => {
+
         const favoriteObject = {
             apartmentId:apartment.id,
             userId: parseInt(localStorage.getItem("reviewApartment_user"))
         }
-        addFavorite(favoriteObject)
+        const apartmentExists = favorites.find(favorite => favorite.apartmentId === apartment.id)
+        /* check if apartment to be added to favorites already 
+        exists in the favorites section*/
+        console.log('apartment exists',apartmentExists)
+        if(apartmentExists){
+            alert(apartment.apartmentName+" apartment already added to favorites")
+        }else{
+            addFavorite(favoriteObject)
+        }
     }
     const logedInUser = localStorage.getItem("reviewApartment_user") 
     console.log('loged ',logedInUser)
@@ -76,7 +85,8 @@ export default ({apartment}) => {
                     <Button color="info" size = "sm">{userComments} comments</Button>
                     <Button color="info" size="sm">Like</Button>
                     <Button color="info" size="sm" onClick = {toggleComment}>Comment</Button>
-                    <Button color="info" size="sm" onClick = {()=> {
+                    <Button color="info" size="sm" onClick = {(evt)=> {
+                        evt.preventDefault()
                         addNewApartmentToFavorites()
                     }}>Add to favorites</Button>
                 </section>
