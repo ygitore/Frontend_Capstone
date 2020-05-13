03d9348a96a1f9e1bfbdf98a74c3be8b4848f657
yitbarekgitore <yitbareky@gmail.com>
2020-05-13 13:32:54 -0500

    implemented user rating

--- src/components/rating/RatingList.js
+++ src/components/rating/RatingList.js
@@ -4,41 +4,46 @@ import {Star} from './Rating'
 import './Rating.css'
 
 export const RatingList = ({apt}) =>{
-    const {addRating} = useContext(RatingContext)
+    const {rating, addRating} = useContext(RatingContext)
 
-    const [rating, setRating] = useState(0);
+    const [userRating, setUserRating] = useState(0);
     const [hoverRating, setHoverRating] = useState(0);
     const stars = [1, 2, 3, 4, 5];
     const userId = parseInt(localStorage.getItem("reviewApartment_user"))
     const addRatingToApi = () => {
-      if(rating !== 0){
+      if(userRating !== 0){
         const ratingObject = {
           "apartmentId":apt.id,
           "userId":userId,
-          "rating":rating+1
+          "rating":userRating+1
         }
         addRating(ratingObject)
       }
     }
-    
-    return (
-      <div className="starRating">
-        <div class="flex-container" onClick = {addRatingToApi }>
-          {stars.map((star, i) => (
-            <Star
-              key={i}
-              starId={i}
-              rating={hoverRating || rating}
-              onMouseEnter={() => setHoverRating(i)}
-              onMouseLeave={() => setHoverRating(0)}
-              onClick={
-                () => 
-                setRating(i)              
-              }
-            />
-          ))}
+    const loggedInUserId = parseInt(localStorage.getItem("reviewApartment_user"))
+    const ratingExists = rating.find(r => r.apartmentId === apt.id && r.userId === loggedInUserId)||{}
+    if(!("userId" in ratingExists)){
+      return (
+        <div className="starRating">
+          <div class="flex-container" onClick = {addRatingToApi }>            
+            {stars.map((star, i) => (
+              <Star
+                key={i}
+                starId={i}
+                rating={hoverRating || rating}
+                onMouseEnter={() => setHoverRating(i)}
+                onMouseLeave={() => setHoverRating(0)}
+                onClick={
+                  () => 
+                  setUserRating(i)              
+                }
+              />
+            ))}
+          </div>
         </div>
-      </div>
-    );
+      );
+    }else{
+      return null
+    }
   }
   
