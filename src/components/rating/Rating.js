43f208a86b9b09de3480c3a57865e1a5ee4c2d5e
yitbarekgitore <yitbareky@gmail.com>
2020-05-16 21:10:46 -0500

    rating is working correctly

--- src/components/rating/Rating.js
+++ src/components/rating/Rating.js
@@ -18,12 +18,12 @@ export const Star = ({ starId, rating, onMouseEnter, onMouseLeave, onClick, apar
       <svg
         height="20px"
         width="25px"
-        class={styleClass}
+        className={styleClass}
         viewBox="0 0 25 23"
         data-rating="1"
       >
         <polygon
-          stroke-width="0"
+          strokeWidth="0"
           points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78"
         />
       </svg>
